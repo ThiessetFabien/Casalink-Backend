07443feb480808e78c7479975ec61b87df921588
larcherlucas <larcher.lucasoclock.school>
2024-05-16 14:05:11 +0200

    fix:environment

--- app/datamappers/users.test.js
+++ app/datamappers/users.test.js
@@ -1,34 +1,91 @@
 import { describe, it } from "mocha";
 import { expect } from "chai";
-import casalinkData from "../services/index";
+import userData from "./user.js";
 
-
-describe('Casalink génére la liste des users', () => {
-    it('should return a object', async () => {
-        expect(typeof casalinkData).to.be.an('object');
-        });
+describe('Casalink generates the list of users', () => {
     it('should return a list of users', async () => {
-        const usersList = await casalinkData.findAllMember();
+        const usersList = await userData.findAllUsers();
         expect(usersList).to.be.an('array');
-        usersList.forEach(member => {
-         expect(member).to.have.property('firstname'); 
-         expect(member).to.have.property('lastname');
-         expect(member).to.have.property('email');
-         expect(member).to.have.property('role');
-         expect(member).to.have.property('status');
-            });
-          });
- it('should return a member by ID', async () => {
-    const memberId = 1; 
-    const member = await casalinkData.findOneMember(memberId);
-    expect(member).to.be.an('object');
-    expect(member).to.have.property('firstname');
-    expect(member).to.have.property('lastname');
-    expect(member).to.have.property('email');
-    expect(member).to.have.property('birthdate');
-    expect(member).to.have.property('role');
-    expect(member).to.have.property('pin');
-    expect(member).to.have.property('score');
-    expect(member).to.have.property('password');
+        usersList.forEach(user => {
+         expect(user).to.have.property('firstname'); 
+         expect(user).to.have.property('lastname');
+         expect(user).to.have.property('email');
+         expect(user).to.have.property('role');
+         expect(user).to.have.property('pin');
+         expect(user).to.have.property('score');
+         expect(user).to.have.property('password');
+         expect(user).to.have.property('home_id');
+        });
+    });
+
+    it('should return a user by ID', async () => {
+        const userId = 1; 
+        const user = await userData.findUserById(userId);
+        expect(user).to.be.an('object');
+        expect(user).to.have.property('firstname');
+        expect(user).to.have.property('lastname');
+        expect(user).to.have.property('email');
+        expect(user).to.have.property('birthdate');
+        expect(user).to.have.property('role');
+        expect(user).to.have.property('pin');
+        expect(user).to.have.property('score');
+        expect(user).to.have.property('password');
+        expect(user).to.have.property('home_id');
+    });
+
+    it('should return a user by home ID', async () => {
+        const homeId = 1; 
+        const users = await userData.findUsersByHomeId(homeId);
+        expect(users).to.be.an('array');
+        users.forEach(user => {
+            expect(user).to.have.property('firstname');
+            expect(user).to.have.property('lastname');
+            expect(user).to.have.property('email');
+            expect(user).to.have.property('birthdate');
+            expect(user).to.have.property('role');
+            expect(user).to.have.property('pin');
+            expect(user).to.have.property('score');
+            expect(user).to.have.property('password');
+            expect(user).to.have.property('home_id');
+        });
+    });
+
+    it('should create a new user', async () => {
+        const newUser = {
+            email: "adrien.Hack@gmail.com",
+            firstname: "Adrien",
+            lastname: "Hack",
+            birthdate: "1990-01-01",
+            role: "adult",
+            pin: "1234",
+            score: 0,
+            password: "helper",
+            home_id: '1'
+        };
+        const user = await userData.createUser(newUser);
+        expect(user).to.be.an('object');
+    });
+
+    it('should update a user', async () => {
+        const userId = 4; 
+        const updateUser = {
+            email: "adrien.Hack@gmail.com",
+            firstname: "IAdrien",
+            lastname: "The Best",
+            birthdate: "1990-01-01",
+            role: "adult",
+            pin: "1234",
+            score: 0,
+            password: "IAhelper",
+            home_id: '1'
+        };
+        const user = await userData.updateUser(userId, updateUser);
+        expect(user).to.be.an('object');
+    });
+    
+  it('should delete a user', async () => {
+    const userId = 4;
+    const result = await userData.deleteUserById(userId);
+    expect(result).to.be.true;
   });
 });
