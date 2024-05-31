31136a62860cdde08fac783f07ff037ff3118aae
Fabien Thiesset <thiessetfabien3@gmail.com>
2024-05-31 10:31:06 +0200

    feat: render database connexion

--- app/datamappers/connexion.js
+++ app/datamappers/connexion.js
@@ -10,7 +10,10 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-  password: process.env.PGPASSWORD
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE
 });
 
 export default pool;
