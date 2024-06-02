5b3fcb339ac5586b134463bad519b57ec5bfd856
Conrath Téo <teo.conrath.pro@gmail.com>
2024-06-02 12:09:26 +0200

    fix: query findProfileByAccountId, date format was wrong

--- app/datamappers/profile.js
+++ app/datamappers/profile.js
@@ -17,9 +17,11 @@ const profilDataMapper = {
   // Find a profile by its account_id
   async findProfileByAccountId(account_id){
     try {
-      const result = await pool.query('SELECT * FROM "profile" WHERE account_id = $1 ORDER BY "name" ASC;', [account_id]);
-      console.log('result', result.rows);
-      return result.rows;
+        //const result = await pool.query('SELECT * FROM "profile" WHERE account_id = $1 ORDER BY "name" ASC;', [account_id]);
+        const result = await pool.query('SELECT id, name, TO_CHAR(birthdate, \'YYYY/MM/DD\') AS birthdate, role, pin, score, image, email, account_id, created_at, updated_at FROM "profile" WHERE account_id = $1 ORDER BY "name" ASC;', [account_id]);
+
+        console.log('date apres traitement de la query', result.rows);
+        return result.rows;
     } catch (error) {
       throw new DbError(error.message);
     }
