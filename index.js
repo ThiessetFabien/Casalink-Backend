18d400867b4eff54b97e6f4e5e08cd28b18af5d9
Conrath Téo <teo.conrath.pro@gmail.com>
2024-06-06 12:17:48 +0200

    improvment: cors option in index.js

--- index.js
+++ index.js
@@ -53,7 +53,9 @@ app.use(bodySanitizer);
 
 createDoc(app);
 
-app.use(cors({ origin: process.env.CORS_ORIGIN }));
+app.use(cors({
+  origin: process.env.CORS_ORIGIN, optionsSuccessStatus: 200, methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 app.use(router);
 
