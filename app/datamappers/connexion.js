28312a6b823a00d0c121f53bcf05da2a422a2753
Alexis Vignot <alexisvignot@hotmail.fr>
2024-06-22 14:43:25 +0200

    fix: ssl

--- app/datamappers/connexion.js
+++ app/datamappers/connexion.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-underscore-dangle */
 import { config } from 'dotenv';
-
-config({ path: `/.env` });
-
 import pg from 'pg';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+config({ path: '/.env' });
 const { Pool } = pg;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const pool = new Pool({
   host: process.env.PGHOST,
@@ -12,6 +17,12 @@ const pool = new Pool({
   url: process.env.PGURL,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
+  ssl: {
+    rejectUnauthorized: true,
+    ca: fs.readFileSync(path.resolve(__dirname, '../certs/root.crt')).toString(),
+    key: fs.readFileSync(path.resolve(__dirname, '../certs/client.key')).toString(),
+    cert: fs.readFileSync(path.resolve(__dirname, '../certs/client.crt')).toString()
+  },
 });
 
 export default pool;
